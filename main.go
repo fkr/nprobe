ff32a6e01b005b12708a3b8b2ad3f54949125939
Felix Kronlage-Dammers <fkr@hazardous.org>
2022-10-19 00:05:03 +0200

    bump version

--- main.go
+++ main.go
@@ -96,8 +96,8 @@ var ConfigFile string
 var Client influxdb2.Client
 var log *logrus.Logger
 
-const version = "0.0.2"
-const apiVersion = "0.0.1"
+const version = "0.0.3"
+const apiVersion = "0.1.0"
 
 func main() {
 
